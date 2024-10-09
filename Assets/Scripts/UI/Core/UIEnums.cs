f1186554370a13e2c3f1fb73ff0cacdeab0a7b89
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-10-09 16:17:47 +0900

    feat: UI Enums 작성
    UI에 쓸 열거형들 작성 하는 스크립트

---
+++ Assets/Scripts/UI/Core/UIEnums.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UIEnums
+{
+    public enum ButtonState
+    {
+        Normal,
+        Hover,
+        Pressed,
+        Disabled
+    }
+}
