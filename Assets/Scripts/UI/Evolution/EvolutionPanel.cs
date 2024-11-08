16e75fa1560385f7c305a361f7574bde783e7058
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-11-08 12:45:36 +0900

    feat: 진화 패널 내 연구 패널 전환 기능 구현

---
+++ Assets/Scripts/UI/Evolution/EvolutionPanel.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EvolutionPanel : MonoBehaviour, ISrollViewControl
+{
+    [Header("Top Button")]
+    [SerializeField] Button queenAntPanelButton;
+    [SerializeField] Button specializePanelButton;
+    [SerializeField] Scrollbar scrollbar;
+    Coroutine currentCoroutine;
+    float scrollSpeed = 10f;
+
+    private void Start()
+    {
+        queenAntPanelButton.onClick.AddListener(() => TopButtonClick(true));
+        specializePanelButton.onClick.AddListener(() => TopButtonClick(false));
+    }
+
+    void TopButtonClick(bool _isQueenAnt)
+    {
+        if (currentCoroutine != null)
+        {
+            StopCoroutine(currentCoroutine);
+        }
+
+        currentCoroutine = StartCoroutine(SlideScrollBar(_isQueenAnt));
+    }
+
+    public IEnumerator SlideScrollBar(bool _isQueenAnt)
+    {
+        while (true)
+        {
+            yield return null;
+
+            if (_isQueenAnt)
+            {
+                scrollbar.value = Mathf.Lerp(scrollbar.value, 0f, Time.deltaTime * scrollSpeed);
+
+                if (Mathf.Approximately(scrollbar.value, 0f))
+                {
+                    scrollbar.value = 0f;
+                    yield break;
+                }
+            }
+            else
+            {
+                scrollbar.value = Mathf.Lerp(scrollbar.value, 1f, Time.deltaTime * 10f);
+
+                if (Mathf.Approximately(scrollbar.value, 1f))
+                {
+                    scrollbar.value = 1f;
+                    yield break;
+                }
+            }
+        }
+    }
+}
