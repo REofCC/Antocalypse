3831a430a523133b8e0fef9252dac99b92fc89bd
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-11-08 20:26:46 +0900

    feat: 연구 노드 SO 작성

---
+++ Assets/Scripts/UI/Research/ResearchNode.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ResearchNode", menuName = "Research/Node")]
+public class ResearchNode : ScriptableObject
+{
+    [SerializeField] Sprite nodeIcon;
+    [SerializeField] string nodeName;
+    [SerializeField] string description;
+    [SerializeField] float progressTime;
+    [SerializeField] int cost;
+    [SerializeField] NodeState nodeState;
+    [SerializeField] List<ResearchNode> nextNodes;
+    [SerializeField] List<ResearchNode> previousNodes;
+    [SerializeField] List<ResearchNode> exclusiveNodes;
+
+
+    public Sprite NodeIcon => nodeIcon;
+    public string NodeName => nodeName;
+    public string Description => description;
+    public float ProgressTime => progressTime;
+    public int Cost => cost;
+    public NodeState NodeState => nodeState;
+    public List<ResearchNode> NextNodes => nextNodes;
+    public List<ResearchNode> PreviousNodes => previousNodes;
+    public List<ResearchNode> ExclusiveNodes => exclusiveNodes;
+
+    public void SetNodeState(NodeState _nodeState)
+    {
+        nodeState = _nodeState;
+    }
+}
