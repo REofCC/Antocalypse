a294ed1d59a5247779cf2047dfee4d9f3ca00c9f
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-12-07 14:18:40 +0900

    feat: ActionButtonController 작성
    비슷한 로직을 수행하는 클래스들의 부모 클래스

---
+++ Assets/Scripts/UI/Core/ActionButtonController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class ActionButtonController : MonoBehaviour
+{
+    [SerializeField] List<Button> buttons = new List<Button>();
+
+    private void Start()
+    {
+        foreach (Button button in buttons)
+        {
+            button.onClick.AddListener(() => OnClickedButton(button));
+        }
+    }
+
+    protected abstract void OnClickedButton(Button button);
+}
