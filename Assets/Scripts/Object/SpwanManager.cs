6d2de3d630901097db606f766d99cd1d92edc99a
REofCC <buzz0131@naver.com>
2024-12-10 01:49:41 +0900

    feat : 병종 기초

--- Assets/Scripts/Object/SpwanManager.cs
+++ Assets/Scripts/Object/SpwanManager.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 public class SpwanManager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
     float spawnTime;
+    GameObject ant;
     public void SpawnAnt(string antType)
     {
         switch (antType)
@@ -23,33 +15,36 @@ public class SpwanManager : MonoBehaviour
                 // break;
                 // else
                 //  currentResource -=cost
-                // spawnTime = _spawnTime 스폰 시간 추가
+                spawnTime = 5.0f; //스폰 시간 추가
+                ant = Resources.Load<GameObject>("Prefabs/Ant/Worker");
                 break;
             case "Scout":
                 // if(cost > currentResource)자원 소비 체크
                 // break;
                 // else
                 //  currentResource -=cost
-                // spawnTime = _spawnTime 스폰 시간 추가
+                spawnTime = 8.0f; //스폰 시간 추가
                 break;
             case "Soldier":
                 // if(cost > currentResource)자원 소비 체크
                 // break;
                 // else
                 //  currentResource -=cost
-                // spawnTime = _spawnTime 스폰 시간 추가
+                spawnTime = 10.0f; //스폰 시간 추가
                 break;
         }
-        StartCoroutine(StartSpawn(spawnTime));
+        StartCoroutine(StartSpawn(spawnTime, ant));
     }
 
-    IEmulator StartSpawn(float timer)
+    IEnumerator StartSpawn(float timer, GameObject ant)
     {
         float t = 0; 
         while (t <= timer)
         {
-            t += time.Deltatime;
+            t += Time.deltaTime;
+            yield return null;
         }
-        //instantiate
+       
+        Instantiate(ant, Vector3.zero, Quaternion.identity);    //스폰 위치 변경 추가
     }
 }
