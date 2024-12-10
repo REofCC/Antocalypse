cee1bc9937074a12721288fc4063c75324f55225
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-12-10 16:43:18 +0900

    feat: ResourcePanel 이벤트 구독
    ResourceManager의 자원 변동 시 이벤트 발생

--- Assets/Scripts/UI/Resource/ResourcePanel.cs
+++ Assets/Scripts/UI/Resource/ResourcePanel.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public enum ResourceType
@@ -22,30 +23,34 @@ public class ResourcePanel : MonoBehaviour
     [SerializeField] TextMeshProUGUI geneticMaterialText;
     [SerializeField] TextMeshProUGUI populationText;
 
-    public void UpdateResource(ResourceType type, int currentAmount, int maxAmount)
+    private void Start()
     {
-        //[LSH:TODO] [USE OBSERVER PATTERN]
-        string displayText = $"{currentAmount}/{maxAmount}";
+        Managers.Resource.OnResourceChange += UpdateResource;
+    }
 
+    private void OnDestroy()
+    {
+        Managers.Resource.OnResourceChange -= UpdateResource;
+    }
+
+    void UpdateResource(ResourceType type)
+    {
         switch (type)
         {
             case ResourceType.LEAF:
-                leafText.text = displayText;
+                leafText.text = $"{Managers.Resource.GetLeaf()}";
                 break;
             case ResourceType.WOOD:
-                woodText.text = displayText;
+                woodText.text = $"{Managers.Resource.GetWood()}";
                 break;
             case ResourceType.LIQUID_FOOD:
-                liquidFoodText.text = displayText;
+                liquidFoodText.text = $"{Managers.Resource.GetLiquidFood()}/{Managers.Resource.GetMaxLiquidFood()}";
                 break;
             case ResourceType.SOLID_FOOD:
-                solidFoodText.text = displayText;
+                solidFoodText.text = $"{Managers.Resource.GetSolidFood()}/{Managers.Resource.GetMaxSolidFood()}";
                 break;
             case ResourceType.GENETIC_MATERIAL:
-                geneticMaterialText.text = displayText;
-                break;
-            case ResourceType.POPULATION:
-                populationText.text = displayText;
+                geneticMaterialText.text = $"{Managers.Resource.GetGenetic()}";
                 break;
             default:
                 Debug.LogError("Invalid resource type");
