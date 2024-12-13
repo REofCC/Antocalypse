15fe4e39e9dafaac828d2db71b1d9c5f0c7c8064
박주현 <yeochi1201@gmail.com>
2024-12-13 23:59:57 +0900

    chore/Resource Data

--- Assets/Scripts/Map/Tool/Util/EnumFile.cs
+++ Assets/Scripts/Map/Tool/Util/EnumFile.cs
@@ -44,19 +44,11 @@ public enum ResourceType
     SOLID_FOOD,
     GENETIC_MATERIAL
 }
-
-public enum ResourceSize
-{
-    Large,
-    Medium,
-    Small
-}
 public enum EventType 
 {
     Question,
     Battle,
     Resource,
-
 }
 
 public enum LayerType
