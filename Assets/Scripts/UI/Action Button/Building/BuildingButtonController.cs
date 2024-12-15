a452b9236455fffcd5f93a21006caf0365b315d5
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-12-15 18:05:18 +0900

    feat: BuildingFactory 이벤트 추가 및 연결
    빌딩 버튼 컨트롤러에 해당 이벤트 연결 후
    interactive 로직 수정

--- Assets/Scripts/UI/Action Button/Building/BuildingButtonController.cs
+++ Assets/Scripts/UI/Action Button/Building/BuildingButtonController.cs
@@ -6,9 +6,46 @@ using UnityEngine.UI;
 
 public class BuildingButtonController : ActionButtonController
 {
+    private void Start()
+    {
+        MapManager.Map.BuildingFactory.OnBuildBuilding += BuildingButtonUnlock;
+        gameObject.GetComponentInParent<SlidePopup>().OnButtonCheck += CheckButtons;
+        InitButtons();
+    }
+
     protected override void OnClickedButton(Button button)
     {
         BuildingType buildingType = button.GetComponent<BuildingButttonData>().GetBuildingName();
         ActiveManager.Active.BuildBuilding(buildingType);
+    }
+
+    protected void BuildingButtonUnlock(BuildingType buildingType)
+    {
+        foreach (Button button in GetButtons())
+        {
+            BuildingButttonData buildingButttonData = button.GetComponent<BuildingButttonData>();
+            CheckButton(button, buildingType, buildingButttonData);
+        }
+    }
+
+    private void CheckButton(Button button, BuildingType buildingType, BuildingButttonData buildingButttonData)
+    {
+        if (buildingType == buildingButttonData.GetBuildingName())
+        {
+            bool isInteractive = MapManager.Map.BuildingFactory.GetBuildingConstraint(buildingType);
+            button.GetComponent<BuildingButttonData>().SetInteractive(isInteractive);
+        }
+    }
+
+    private void CheckButtons()
+    {
+        foreach (Button button in GetButtons())
+        {
+            bool isInteractive = button.GetComponent<BuildingButttonData>().GetInteractive();
+            if (!isInteractive)
+            {
+                button.GetComponent<BuildingButttonData>().SetInteractive(isInteractive);
+            }
+        }
     }
 }
