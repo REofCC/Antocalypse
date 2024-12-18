29182d63b836ce477b4f1ddc2773ffd768390c09
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-12-18 20:41:04 +0900

    fix: 레이캐스트 null값 방지

--- Assets/Scripts/UI/Popup/PopupEventTriggerWorldSpace.cs
+++ Assets/Scripts/UI/Popup/PopupEventTriggerWorldSpace.cs
@@ -25,13 +25,13 @@ public class PopupEventTriggerWorldSpace : MonoBehaviour
     }
 
     private void HandleMouseInput()
-    {        
+    {
         Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero,Mathf.Infinity, layer);
-            if (hit.collider.gameObject == null)
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero, Mathf.Infinity, layer);
+            if (hit.collider == null)
             {
                 return;
             }
